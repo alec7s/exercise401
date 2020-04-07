d5b6216109558c0ce76ce3ecc3aab1f600ec2e92
Alex Evans <alex.c.evans1@gmail.com>
2020-04-06 20:55:11 -0400

    Initial commit

---
+++ src/main/java/com/ae/exercise401/Exercise401Application.java
@@ -0,0 +1,13 @@
+package com.ae.exercise401;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Exercise401Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Exercise401Application.class, args);
+    }
+
+}
